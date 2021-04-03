19e87bc78f603ac354c5f659c837f121366030ac
Luiz Gabriel Deganutti <deganutti@outlook.com.br>
2021-04-03 10:06:06 -0300

    Projeto Inicial
    Sem rotas complexas, dados fixos sem movimentação em base de dados

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const routes = express.Router();
+
+const views = __dirname + '/views/';
+
+const profile = {
+    'name': 'Luiz Gabriel Deganutti',
+    'avatar': 'https://avatars.githubusercontent.com/u/18487517?v=4',
+    'monthly-budget': 4000,
+    'hours-per-day': 7,
+    'days-per-week': 6,
+    'vacation-per-year': 4,
+}
+
+
+
+
+
+routes.get('/', (req, res) => {
+    return res.render(views + '/index');
+});
+
+routes.get('/job', (req, res) => {
+    return res.render(views + '/job');
+});
+routes.get('/job-edit', (req, res) => {
+    return res.render(views + '/job-edit');
+});
+
+routes.get('/profile', (req, res) => {
+    return res.render(views + '/profile', { profile: profile });
+});
+
+
+
+module.exports = routes;
