9ce3b37689235fa866ec8e492111ee5152ec3b9b
Luiz Gabriel Deganutti <deganutti@outlook.com.br>
2021-04-03 21:37:16 -0300

    ultimos ajustes aula2

--- src/routes.js
+++ src/routes.js
@@ -61,26 +61,27 @@ const Job = {
             'name': 'Novo aplicativo controle financeiro',
             'daily-hours': 3,
             'total-hours': 1,
-            'createdAt': Date.now(),
+            'createdAt': Date.now(), 
         },
         {
             'id': 2,
             'name': 'Ajuste login cripto',
             'daily-hours': 3,
             'total-hours': 60,
-            'createdAt': Date.now(),
+            'createdAt': Date.now(), 
         }
     ],
     controllers:{
         index(req, res) {
             const updatedJobs = Job.data.map((job) =>{
                 const remaining  = Job.services.remainingDays(job);
+               
                 const status = remaining <= 0 ? 'done' : 'progress';
                 return {
                     ...job,
                     remaining,
                     status,
-                    budget: Profile.data['price-hours']*job['total-hours']
+                    budget: Job.services.calculateBudget(job,Profile.data['price-hours'])
                 };
             });       
         
@@ -92,41 +93,93 @@ const Job = {
        
                     // req.body = { name: 'asdas', 'daily-hours': '3', 'total-hours': '90' },
                 
-                    const lastId = Job.data[Job.data.length - 1]?.id || 1;
+                    const lastId = Job.data[Job.data.length - 1]?.id || 0;
                     const jobs = req.body;
                       
                     jobs.createdAt = Date.now(); //atribuição de data de criação da tarefa em expecifico - milisegundos;
                 
                     Job.data.push({
-                        'id': lastId,
+                        'id': lastId+1,
                         'name': req.body['name'],
                         'daily-hours': req.body['daily-hours'],
                         'total-hours': req.body['total-hours'],
-                        'createdAd': req.body['createdAt'],
+                        'createdAt': req.body['createdAt'],
                     });
                 
                     return res.redirect('/');             
         },
         create(req,res){
             return res.render(views + '/job');
+        },
+        show(req,res){ 
+
+            const jobId = req.params.id;
+            
+            const job = Job.data.find(job => Number(job.id) === Number(jobId));
+            if(!job){
+                return res.send('Job Not found');
+            } 
+
+            //valor do JOB
+            job.budget = Job.services.calculateBudget(job, Profile.data['price-hours']);
+
+            return res.render(views + '/job-edit',{ job }); 
+
+        },
+        update(req,res){
+            const jobId = req.params.id;
+            
+            const job = Job.data.find(job => Number(job.id) === Number(jobId));
+            if(!job){
+                return res.send('Job Not found');
+            };
+            
+            const updatedJob = {
+                ...job,//espalhando os valores de JOB
+                'name':req.body['name'],
+                'total-hours':req.body['total-hours'],
+                'daily-hours':req.body['daily-hours'],
+            };
+
+
+            //atualizar os dados no array 
+            Job.data = Job.data.map(job =>{
+                if(Number(job.id) === Number(jobId)){
+                    job = updatedJob;
+                }
+                return job;
+            });
+
+            res.redirect('/job/'+jobId); 
+        },
+        delete(req,res){
+            const jobId = req.params.id;
+            
+            Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId));
+
+            res.redirect('/'); 
         }
     },
     services:{
          remainingDays(job){
             const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed();
-        
+           
             const createdDate = new Date(job['createdAt']);
-            const dueDay = createdDate.getDate()+Number(remainingDays); //dia do vencimento
-            const dueDate = createdDate.setDate(dueDay);
-            const timeDiffInMs = dueDate - Date.now();
+            const dueDay = createdDate.getDate() + Number(remainingDays); //dia do vencimento
+            const dueDateInMs = createdDate.setDate(dueDay);
+            const timeDiffInMs = dueDateInMs - Date.now();
             // transformar os milisegundo para dias.
         
             const dayInMs = 1000 * 60 * 60 * 24;
+            console.log(job['createdAt']);
+            const dayDiff = Math.floor(timeDiffInMs / dayInMs);
         
-            const dayDiff = (timeDiffInMs / dayInMs).toFixed();
-        
+            
+
             return dayDiff;
-        }
+        },
+        calculateBudget:(job,priceHours)=>job['total-hours'] * priceHours ,
+        
         
 
     },
@@ -145,9 +198,11 @@ routes.post('/job', Job.controllers.save);
 
 routes.get('/job', Job.controllers.create);
 
-routes.get('/job-edit', (req, res) => {
-    return res.render(views + '/job-edit');
-});
+routes.post('/job/:id', Job.controllers.update);
+
+routes.post('/job/delete/:id', Job.controllers.delete);
+
+routes.get('/job/:id', Job.controllers.show);
 
 routes.get('/profile', Profile.controllers.index);
 routes.post('/profile', Profile.controllers.update);
